578fc54e826f2bea2142980e15cf8de64fa02ef5
Sergio Garcia <sergio@MacBook-Pro-de-Sergio-2.local>
2020-02-07 11:30:08 -0600

    Deleted git branch from client

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://sergio:sergio123@cluster0-ocs1i.mongodb.net/test?retryWrites=true&w=majority'     
-};
--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const items = require('./routes/api/items');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
 
 const app = express();
 
-//Bodyparser middleware
-app.use(bodyParser.json());
+//Bodyparser Middleware
+app.use(express.json());
 
 //DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get("mongoURI");
 
-//Connect to MONGO 
+//Connect to Mongo
 mongoose
-    .connect(db,{useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true})
-    .then(() => console.log('MongoDB Connected ...'))
-    .catch(err => console.log(err));
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("MongoDB Connected ..."))
+  .catch(err => console.log(err));
 
-//Use Routes 
-app.use('/api/items', items);
+//Use Routes
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
-//Serve static assests if in production 
-if (process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
+//Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 5000;
